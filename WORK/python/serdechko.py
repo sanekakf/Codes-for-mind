d18d8a61a947e9d9d4e1b2576bf1a25c7c393466
Sanekakf <83131846+sanekakf@users.noreply.github.com>
2022-03-02 10:22:37 +0300

    Create serdechko.py

---
+++ WORK/python/serdechko.py
@@ -0,0 +1,24 @@
+from time import sleep
+from turtle import *
+sleep(3)
+left(90)
+pensize(10)
+penup()
+forward(100)
+pendown()
+pencolor("red")
+begin_fill()
+circle(70,230)
+pensize(10)
+pencolor("red")
+
+pencolor("red")
+forward(140)
+seth(40)
+forward(135)
+pencolor("red")
+right(5)
+circle(70,210)
+pencolor("black")
+input()
+
